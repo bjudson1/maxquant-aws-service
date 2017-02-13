c99b86b95e088789060f91952406afe4f690b35c
Garrett McGrath <Garrett McGrath>
2017-02-12 21:25:40 -0500

    Add pip files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'maxquant-aws-service',
+  packages = ['maxquant-aws-service'],
+  version = '0.1',
+  description = '',
+  author = 'Brenden Judson',
+  author_email = 'bjudson1@nd.edu',
+  url = 'https://github.com/bjudson1/maxquant-aws-service',
+  download_url = 'https://github.com/bjudson1/maxquant-aws-service/tarball/0.1',
+  keywords = ['maxquant', 'aws', 'service'],
+  classifiers = [],
+)
